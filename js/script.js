612d10ef89080245da0a2d64cf9a5b3c3af0f2d8
Your Name <you@example.com>
2021-01-08 22:39:35 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log('salamalejkum')
+const answers = [];
+answers[0] = prompt('kak twoje imja?', '');
+answers[1] = prompt('kak twoje familja?', '');
+answers[2] = prompt('kak twoje rost?', '');
+
+console.log(typeof(answers));
+const user = 'Alex';
+alert(`priwet ${user}`);
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+console.log(2*2 == 4);
+ 
