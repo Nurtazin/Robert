4662e437daa7d11e4a0931e0de6b2f139ee93172
Your Name <you@example.com>
2021-01-09 16:07:56 +0100

    script.js uslowija

--- js/script.js
+++ js/script.js
@@ -1,15 +1,40 @@
-const MyMobFilm = +prompt('scolko filmov ty posmotrel?' , ' ');
-const personalMd = {
-  count: MyMobFilm,
-  movies:{},
-  actors:{},
-  genres:[],
-  privat: false
-};
-const a = prompt('poslednij prosmotrennyj film' , ''),
-      b = prompt('kak wy jego oceniwajjete?' , ''),
-      c = prompt('poslednij prosmotrennyj film' , ''),
-      d = prompt('kak wy jego oceniwajjete?' , '');
-personalMd.movies[a] = b;
-personalMd.movies[c] = d;
-console.log(personalMd);
+//const MyMobFilm = +prompt('scolko filmov ty posmotrel?' , ' ');
+//const personalMd = {
+ // count: MyMobFilm,
+ // movies:{},
+ // actors:{},
+ // genres:[],
+ // privat: false
+//};
+//const a = prompt('poslednij prosmotrennyj film' , ''),
+//     b = prompt('kak wy jego oceniwajjete?' , ''),
+//      c = prompt('poslednij prosmotrennyj film' , ''),
+//     d = prompt('kak wy jego oceniwajjete?' , '');
+//personalMd.movies[a] = b;
+//personalMd.movies[c] = d;
+//console.log(personalMd);
+//if ( 4 == 4 ){
+//    console.log('ok');
+//};
+//const rar = 50;
+//if(50 == 49){
+ //   console.log('no');
+//} else if (50 == 50){
+ //   console.log('ok');
+//};
+//(rar === 50) ? console.log('ok') : console.log('error');
+//const jaj = 60
+//switch(jaj){
+ //   case 90:
+//        console.log('newerno');
+//        break;
+//        case 90:
+//            console.log('newerno');
+//            break;
+ //           case 61:
+ //               console.log('yes');
+//                break;
+//                default:
+//                    console.log('ne w etot raz');
+//                    break;
+//}
