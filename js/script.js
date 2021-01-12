dfc41d5941f5b8e2d0dccbd54621515b02b270bc
Your Name <you@example.com>
2021-01-12 18:18:21 +0100

    practik4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,179 @@
 'use strict';
+const personalMovieDB = {
+        count: 0,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false,
+        start : function() {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+                while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+                    personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+                }
+            },
+        rememberMyFilms: function(){
+            for ( let i = 0; i < 2; i++ ){
+                     const a =prompt('poslednij prosmotrennyj film' , ''),
+                           b = prompt('kak wy jego oceniwajjete?' , '');
+                     if(a != null && b!=null && a!= '' && b!= '' && a.length < 50) {
+                    personalMd.movies[a] = b;
+                        console.log('done');
+                   }
+                    else {
+                        console.log('none');
+                       i--;
+                    }
+                }
+          },
+          detectorPersonalLevel: function(){
+           if (personalMovieDB.count < 10){
+               console.log("zamalo");
+           } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+               console.log("clasikin");
+           } else if (personalMovieDB.count >=30){
+               console.log("krasawa");
+           } else {
+               console.log("error");
+           }
+          },
+          showMyDB: function(hidden){
+            if(!hidden){
+                    console.log(personalMovieDB);
+          }
+          },
+          toggleVisibleMyDB: function(){
+             if (personalMovieDB.privat){
+                personalMovieDB.privat = false;
+             } else{
+                personalMovieDB.privat = true;
+             }
+          },
+          writeyuergenres: function(){
+            for(let i = 1 ; i <= 3; i++ ){
+                        const genre = prompt (`wasz lubimyj rzanrpod nomerom ${i}`);
+                        if(genre === "" || genre == null){
+                           console.log("wy wweli nekorektnyje dannyje")
+                           i--;
+                        } else{
+                        personalMovieDB.genres[i - 1] = genre
+                        }
+                    }
+            personalMovieDB.genres.forEach((item, i) =>{
+                console.log(`Любимый жанр ${i+1} - это ${item}`);
+            });
+          }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//osnowy oop start
+// console.dir(['1', '2', '3']);
+
+// const solider = {
+//     health: 400,
+//     armor: 400,
+//     sayHello: function(){
+//         console.log("hello");
+//     }
+// };
+// const maikl = Object.create(solider);
+// console.log(maikl);
+// const roloik = {
+//     health: 100
+// };
+
+// Object.setPrototypeOf(roloik, solider)
+
+// roloik.sayHello();
+//osnowy oop finish
+//copy start
+// function copy (mainobj){
+//     let objm = {};
+//     let key;
+//     for(key in mainobj){
+//         objm[key] = mainobj[key];
+//     };
+//      return objm;
+// }
+//   const numbers = {
+//       a: 5,
+//       b: 6,
+//       c: 8,
+//       d:{
+//            x: 1,
+//            y: 12
+//       }
+//   };
+//   const NewNumbers = copy(numbers);
+//   NewNumbers.a = 10;
+//   console.log(NewNumbers);
+//   console.log(numbers);
+
+// const art = {
+//     w: 4,
+//     b:{
+//         h:5,
+//         c:2
+//     }
+// };
+// const uty = {
+//     r:5,
+//     l:12
+// };
+// const clone = Object.assign({}, uty);
+// clone.r = 45;
+// console.log(clone);
+
+
+// //console.log(Object.assign(art, uty));
+// const oldArray = ['a',  'b', 'g', 't', 'u'];
+// const newArray = oldArray.slice();
+// newArray[3] = 'asdasadasda';
+// console.log(oldArray);
+// console.log(newArray);
+
+// const app = ['kkl', 'warcraft', 'wedmak', 'toys'],
+//       soc = ['vk', 'instagram', 'youtube', 'play'],
+//       alls = [...app, ...soc, 'rrrrr', 'ooooo'];
+// console.log(alls);
+
+// function log(a, b, c){
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// };
+// const num = ['5', '6', '7'];
+// log(...num);
+//copy finish
 //massiv start
 // const str = prompt("" , "")
 // const produkts = str.split(", ")
@@ -72,6 +247,7 @@
 //      d = prompt('kak wy jego oceniwajjete?' , '');
 //personalMd.movies[a] = b,
 //personalMd.movies[c] = d;
+
 //for ( let i = 0; i < 2; i++ ){
 //    const a =prompt('poslednij prosmotrennyj film' , ''),
 //          b = prompt('kak wy jego oceniwajjete?' , '');
